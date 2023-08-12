2cc63d9fe52174932c99173a648d35bbd55b4816
nikolaytswetanow <nikolaytswetanow@gmail.com>
2023-08-12 15:25:53 +0300

    updated ui tests' links

--- tests/ui/boardgames.test.js
+++ tests/ui/boardgames.test.js
@@ -1,7 +1,7 @@
 const { test, expect } = require('@playwright/test');
 
 test('Check boardgames page', async ({ page }) => {
-    await page.goto('http://localhost:8080/collection');
+    await page.goto('https://nikolaytswetanow-exam.onrender.com/boardgames');
     const list = await page.$('ul');
     expect(list).toBeTruthy();
   });
--- tests/ui/home.test.js
+++ tests/ui/home.test.js
@@ -1,9 +1,9 @@
 const { test, expect } = require('@playwright/test');
 
 test('Check home page', async ({ page }) => {
-    await page.goto('http://localhost:8080/');
+    await page.goto('https://nikolaytswetanow-exam.onrender.com/');
     const heading = await page.$('h1');
     const text = await heading.textContent();
-    expect(text).toContain('Welcome');  
+    expect(text).toContain('Boardgames Collection');  
   });
   
